5abb6caaf4a502ebbc5a81b408823e4267ba59d4
ab93 <avikbasu93@gmail.com>
2015-11-11 13:10:02 -0800

    latest

--- mancala.py
+++ mancala.py
@@ -9,6 +9,7 @@ MAX_PLAYER = 1
 MIN_PLAYER = 2
 MAX_VAL = -inf
 ChosenState = None
+isGreedy = False
 
 class GameState:
 
@@ -68,7 +69,6 @@ class GameState:
     def isGameOver(self):
         return self.gameOver
 
-    ## add gameover condition
 
 def printTraverseLog_alpha_beta(node,depth,value,isNextMove,alpha,beta):
     global inf
@@ -89,17 +89,23 @@ def printTraverseLog_alpha_beta(node,depth,value,isNextMove,alpha,beta):
         elif beta == inf:
             beta = 'Infinity'
 
-        print "node,depth,value,isNextMove,alpha,beta:",node + ',' + str(depth) + ',' + str(value) + ',' + str(isNextMove) + ',' + str(alpha) + ',' + str(beta) + '\n'
+        #print "node,depth,value,isNextMove,alpha,beta:",node + ',' + str(depth) + ',' + str(value) + ',' + str(isNextMove) + ',' + str(alpha) + ',' + str(beta) + '\n'
         f.write(node + ',' + str(depth) + ',' + str(value) + ',' + str(alpha) + ',' + str(beta) + '\n')
 
 
 def printTraverseLog(node,depth,value,isNextMove):
+
+    global isGreedy
+
+    if isGreedy == True:
+        return
+
     with open('traverse_log.txt','a+') as f:
         if value == inf:
             value = 'Infinity'
         elif value == -inf:
             value = '-Infinity'
-        print "node,depth,value,isNextMove:",node + ',' + str(depth) + ',' + str(value) + ',' + str(isNextMove) + '\n'
+        #print "node,depth,value,isNextMove:",node + ',' + str(depth) + ',' + str(value) + ',' + str(isNextMove) + '\n'
         f.write(node + ',' + str(depth) + ',' + str(value) + '\n')
 
 
@@ -129,7 +135,7 @@ def AlphaBeta(State,player,cutoff_depth):
 
         global MAX_VAL,ChosenState
 
-        if ((depth == cutoff_depth and isNextMove == False) or (State.gameOver == True)):
+        if ((depth == cutoff_depth and isNextMove == False) or State.gameOver == True ):
             State.setEvalScore()
             printTraverseLog_alpha_beta(State.getNode(),depth,State.getEvalScore(),isNextMove,alpha,beta)
 
@@ -137,25 +143,19 @@ def AlphaBeta(State,player,cutoff_depth):
                 if State.getEvalScore() > MAX_VAL:
                     ChosenState = State
                     MAX_VAL = State.getEvalScore()
-                answerStates.append(State)
-                values.append(State.getEvalScore())
 
             return State.getEvalScore()
-
-        #raw_input()
 
         index = 0
         if isNextMove == False:
             v = -inf
+
             legalSuccessors,nextMoves = GetLegalSuccessors(State,MAX_PLAYER)
 
             for action in legalSuccessors:
                 printTraverseLog_alpha_beta(State.getNode(),depth,v,isNextMove,alpha,beta)
                 v = max(v, minValue(action,depth + 1,cutoff_depth,nextMoves[index],alpha,beta))
                 index += 1
-
-                #print "now....Node,depth,v:",State.getNode(),depth,v
-                #raw_input()
 
                 if v >= beta:
                     printTraverseLog_alpha_beta(State.getNode(),depth,v,isNextMove,alpha,beta)
@@ -164,6 +164,7 @@ def AlphaBeta(State,player,cutoff_depth):
 
         else:
             v = inf
+
             legalSuccessors,nextMoves = GetLegalSuccessors(State,MIN_PLAYER)
 
             for action in legalSuccessors:
@@ -182,8 +183,6 @@ def AlphaBeta(State,player,cutoff_depth):
             if v > MAX_VAL:
                 ChosenState = State
                 MAX_VAL = v
-            answerStates.append(State)
-            values.append(v)
 
         return v
 
@@ -192,7 +191,7 @@ def AlphaBeta(State,player,cutoff_depth):
 
         global MAX_VAL,ChosenState
 
-        if ((depth == cutoff_depth and isNextMove == False) or (State.gameOver == True)):
+        if ((depth == cutoff_depth and isNextMove == False) or State.gameOver == True):
             State.setEvalScore()
             printTraverseLog_alpha_beta(State.getNode(),depth,State.getEvalScore(),isNextMove,alpha,beta)
 
@@ -200,8 +199,6 @@ def AlphaBeta(State,player,cutoff_depth):
                 if State.getEvalScore() > MAX_VAL:
                     ChosenState = State
                     MAX_VAL = State.getEvalScore()
-                answerStates.append(State)
-                values.append(State.getEvalScore())
 
             return State.getEvalScore()
 
@@ -209,6 +206,7 @@ def AlphaBeta(State,player,cutoff_depth):
 
         if isNextMove == False:
             v = inf
+
             legalSuccessors,nextMoves = GetLegalSuccessors(State,MIN_PLAYER)
             for action in legalSuccessors:
                 printTraverseLog_alpha_beta(State.getNode(),depth,v,isNextMove,alpha,beta)
@@ -222,6 +220,7 @@ def AlphaBeta(State,player,cutoff_depth):
 
         else:
             v = -inf
+
             legalSuccessors,nextMoves = GetLegalSuccessors(State,MAX_PLAYER)
             for action in legalSuccessors:
                 printTraverseLog_alpha_beta(State.getNode(),depth,v,isNextMove,alpha,beta)
@@ -239,8 +238,6 @@ def AlphaBeta(State,player,cutoff_depth):
             if v > MAX_VAL:
                 ChosenState = State
                 MAX_VAL = v
-            answerStates.append(State)
-            values.append(v)
 
         return v
 
@@ -270,55 +267,20 @@ def AlphaBeta(State,player,cutoff_depth):
 
     printTraverseLog_alpha_beta(State.getNode(),0,bestScore,False,alpha,beta)
 
-    print "\nChosen Action:"
-    bestSuccessor.display()
+    #print "\nChosenState:",ChosenState.display()
 
-    #i = 0
-    #for state in answerStates:
-    #    print '\n'
-    #    state.display()
-    #    print values[i]
-    #    i += 1
-
-    print "Bestscore:",bestScore
-
-    i=0
-    temp = []
-    for state in answerStates:
-        print values[i]
-        if values[i] == bestScore and state != bestSuccessor :
-            temp.append(state)
-        i += 1
-
-    if len(temp) == 0:
-        finalNextState = bestSuccessor
-
-    else:
-        print "\ntemp states:"
-        MAX_EVAL = -inf
-        for state in temp:
-            if state.getEvalScore() > MAX_EVAL:
-                MAX_EVAL = state.getEvalScore()
-                finalNextState = state
-            state.display()
-
-    print "\nFinal Next State:"
-    finalNextState.display()
-
-    print "\nChosenState:",ChosenState.display()
-
-    printNextState(finalNextState)
+    printNextState(ChosenState)
 
 
 def Minimax(State,player,cutoff_depth):
 
-    global MAX_PLAYER,MIN_PLAYER,MAX_VAL,ChosenState
+    global MAX_PLAYER,MIN_PLAYER,MAX_VAL,ChosenState,isGreedy
 
     def maxValue(State,depth,cutoff_depth,isNextMove):
 
         global MAX_VAL,ChosenState
 
-        if ((depth == cutoff_depth and isNextMove == False) or (State.gameOver == True)):
+        if ((depth == cutoff_depth and isNextMove == False) or State.gameOver == True):
             State.setEvalScore()
             printTraverseLog(State.getNode(),depth,State.getEvalScore(),isNextMove)
 
@@ -326,19 +288,13 @@ def Minimax(State,player,cutoff_depth):
                 if State.getEvalScore() > MAX_VAL:
                     ChosenState = State
                     MAX_VAL = State.getEvalScore()
-                answerStates.append(State)
-                values.append(State.getEvalScore())
 
             return State.getEvalScore()
-
-            #if isNextMove == False:
-                #printTraverseLog(State.getNode(),depth,State.getEvalScore(),isNextMove)
-                #return State.getEvalScore()
 
         index = 0
         if isNextMove == False:
             v = -inf
-            #legalSuccessors,nextMoves = GetLegalSuccessors(State,1)
+
             legalSuccessors,nextMoves = GetLegalSuccessors(State,MAX_PLAYER)
             for action in legalSuccessors:
                 printTraverseLog(State.getNode(),depth,v,isNextMove)
@@ -346,8 +302,7 @@ def Minimax(State,player,cutoff_depth):
                 index += 1
         else:
             v = inf
-            #if depth == cutoff_depth:
-            #    v = State.getEvalScore()
+
             legalSuccessors,nextMoves = GetLegalSuccessors(State,MIN_PLAYER)
             for action in legalSuccessors:
                 printTraverseLog(State.getNode(),depth,v,isNextMove)
@@ -359,39 +314,30 @@ def Minimax(State,player,cutoff_depth):
             if v > MAX_VAL:
                 ChosenState = State
                 MAX_VAL = v
-            answerStates.append(State)
-            values.append(v)
-        print "max_v,depth:",v,depth
+
         return v
 
 
     def minValue(State,depth,cutoff_depth,isNextMove):
 
         global MAX_VAL,ChosenState
-        #State.display()
-        #print (State.gameOver)
-        #raw_input()
 
-        if ((depth == cutoff_depth and isNextMove == False) or (State.gameOver == True)) :
+        if ((depth == cutoff_depth and isNextMove == False) or State.gameOver == True):
             State.setEvalScore()
             printTraverseLog(State.getNode(),depth,State.getEvalScore(),isNextMove)
-            #raw_input()
 
             if depth == 1:
                 if State.getEvalScore() > MAX_VAL:
                     ChosenState = State
                     MAX_VAL = State.getEvalScore()
-                answerStates.append(State)
-                values.append(State.getEvalScore())
 
             return State.getEvalScore()
 
         index= 0
 
-        #raw_input("wrong place")
-
         if isNextMove == False:
             v = inf
+
             legalSuccessors,nextMoves = GetLegalSuccessors(State,MIN_PLAYER)
             for action in legalSuccessors:
                 printTraverseLog(State.getNode(),depth,v,isNextMove)
@@ -399,29 +345,24 @@ def Minimax(State,player,cutoff_depth):
                 index += 1
         else:
             v = -inf
-            #if depth == cutoff_depth:
-            #    v = State.getEvalScore()
+
             legalSuccessors,nextMoves = GetLegalSuccessors(State,MAX_PLAYER)
             for action in legalSuccessors:
                 printTraverseLog(State.getNode(),depth,v,isNextMove)
                 v = max(v, minValue(action,depth,cutoff_depth,nextMoves[index]))
                 index += 1
 
-        print "min_v,depth:",v,depth
         printTraverseLog(State.getNode(),depth,v,isNextMove)
         if depth == 1:
             if v > MAX_VAL:
                 ChosenState = State
                 MAX_VAL = v
-                print MAX_VAL
-                ChosenState.display()
-                #raw_input()
-            answerStates.append(State)
-            values.append(v)
+
         return v
 
-    f= open('traverse_log.txt','w')
-    f.close()
+    if isGreedy == False:
+        f= open('traverse_log.txt','w')
+        f.close()
 
     printTraverseLog('Node','Depth','Value','')
     legalSuccessors,nextMoves = GetLegalSuccessors(State,MAX_PLAYER)
@@ -439,93 +380,11 @@ def Minimax(State,player,cutoff_depth):
             bestSuccessor = action
             bestScore = score
 
-    print "\nChosen Action:"
     printTraverseLog(State.getNode(),0,bestScore,False)
 
-    bestSuccessor.display()
-    #answerStates.append(State)
-
-    print "\ndepth 1 states:"
-    i = 0
-    for state in answerStates:
-        print '\n'
-        state.display()
-        print values[i]
-        i += 1
-
-    print "Bestscore:",bestScore
-
-    i=0
-    temp = []
-    for state in answerStates:
-        print values[i]
-        if values[i] == bestScore and state != bestSuccessor :
-            temp.append(state)
-        i += 1
-
-    if len(temp) == 0:
-        finalNextState = bestSuccessor
-
-    print "\ntemp states:"
-    MAX_EVAL = -inf
-    for state in temp:
-        if state.getEvalScore() > MAX_EVAL:
-            MAX_EVAL = state.getEvalScore()
-            finalNextState = state
-        state.display()
-
-    print "\nFinal Next State:"
-    finalNextState.display()
-
-    print '\nChosenState:',ChosenState.display()
+    #print '\nChosenState:',ChosenState.display()
 
     printNextState(ChosenState)
-
-
-
-def Greedy(State,player):
-
-    actions,nextMoves = GetLegalSuccessors(State,player)
-    nextMoves_rev = list(reversed(nextMoves))
-
-    if player == 1:
-        maxi = -inf
-        i = -1
-        for action in actions:
-            i += 1
-            if action.getEvalScore() > maxi:
-                maxi = action.getEvalScore()
-                newState = action
-                index = i
-
-        print "\nchosen index:",index
-        print "Chosen state:"
-
-        newState.display()
-
-        if nextMoves[index] == True:
-            Greedy(newState,player)
-
-    else:
-        mini = inf
-        #i = len(actions)
-        i = -1
-        #for action in list(reversed(actions)):
-        for action in actions:
-            i += 1
-            if action.getEvalScore() < mini:
-                mini = action.getEvalScore()
-                newState = action
-                index = i
-
-        print "\nchosen index:",index
-        print "Chosen state:"
-
-        newState.display()
-
-        if nextMoves[index] == True:
-            print "\nHas an extra turn"
-            Greedy(newState,player)
 
 
 def CheckGameEnd(P1_Stones,P2_Stones,pitSize):
@@ -540,7 +399,6 @@ def CheckGameEnd(P1_Stones,P2_Stones,pitSize):
             break
 
     if all_zeros == True:   #Make P2 pits to 0 and transfer to its mancala
-        print "P1 Empty"
         value = 0
         for i in range(1,pitSize+1):
             value += P2_Stones_copy[i]
@@ -556,7 +414,6 @@ def CheckGameEnd(P1_Stones,P2_Stones,pitSize):
             break
 
     if all_zeros == True:    #Make P1 pits to 0 and transfer to its mancala
-        print
         value = 0
         for i in range(pitSize):
             value += P1_Stones_copy[i]
@@ -580,8 +437,6 @@ def GetLegalSuccessors(state,player):
     P1_Mancala = state.getP1_Mancala()
     P2_Stones = state.getP2_Stones()
     P2_Mancala = state.getP2_Mancala()
-
-    print P2_Stones,P1_Stones
 
     if player == 1:    #only for player 1
         pitSize = state.getPitSize()
@@ -622,33 +477,20 @@ def GetLegalSuccessors(state,player):
             P1_Stones_new = pits[:pitSize+1]
             P2_Stones_new = [P2_Mancala] + list(reversed(pits[pitSize+1:]))
 
-            #print P1_Stones_new,P2_Stones_new
-            #raw_input("before:")
-
             P1_Stones_new,P2_Stones_new,isGameEnd = CheckGameEnd(P1_Stones_new,P2_Stones_new,pitSize)
-
-            #print P1_Stones_new,P2_Stones_new
-            #raw_input("after:")
 
             newState = GameState(P2_Stones_new,P1_Stones_new)
 
             if isGameEnd == True:
                 newState.setGameOver()
-                isNextMove = False
-                print "End move:"
-                newState.display()
-                #raw_input()
 
             possibleStates.append(newState)
             nextMove.append(isNextMove)
 
-        print nextMove,nodes
         i = 0
         for x in possibleStates:
-            print "\nPossible States:"
             x.setEvalScore()
             x.setNode(nodes[i])
-            x.display()
             i += 1
 
         return possibleStates,nextMove
@@ -657,19 +499,14 @@ def GetLegalSuccessors(state,player):
         pitSize = state.getPitSize()
 
         for pitIndex in range(pitSize):
-            #print "\npitIndex:",pitIndex
             isNextMove = False
             pits = list(reversed(P2_Stones)) + P1_Stones[:-1]
-
-            #print "pits initially:",pits
 
             if pits[pitIndex] == 0:
                 continue
             else:
-                #nodes.append('A'+str(pitIndex + 2))
                 nodes.append('A'+str(pitSize - pitIndex + 1))
                 val = pits[pitIndex]
-                #print val
                 pits[pitIndex] = 0
                 i = pitIndex + 1
 
@@ -680,9 +517,7 @@ def GetLegalSuccessors(state,player):
                         pits[pitSize] += 1
                         i += 1
                         val -= 1
-                        #print "pits now:",pits
                     else:
-                        #print pits,i
                         if i > len(pits)-1:
                             i = 0
                             continue
@@ -690,9 +525,7 @@ def GetLegalSuccessors(state,player):
                             pits[i] += 1
                             i += 1
                             val -= 1
-                        #print "pits now:",pits
 
-                #print "i,pitsize:",i-1,pitSize
                 if i - 1 == pitSize:
                     isNextMove = True
 
@@ -705,26 +538,15 @@ def GetLegalSuccessors(state,player):
 
             if isGameEnd == True:
                 newState.setGameOver()
-                isNextMove = False
-                print "End move:"
-                newState.display()
-                #raw_input()
 
             possibleStates.append(newState)
             nextMove.append(isNextMove)
 
-            #print "pits:",pits
-
-        print "\n",list(reversed(nextMove)),list(reversed(nodes))
         i = len(possibleStates) - 1
         for x in list(reversed(possibleStates)):
-            print "\nPossible States:"
             x.setEvalScore()
             x.setNode(nodes[i])
-            x.display()
             i -= 1
-
-        #raw_input()
 
         return list(reversed(possibleStates)),list(reversed(nextMove))
 
@@ -733,14 +555,12 @@ def GetLegalSuccessors(state,player):
 def ParseInputFile():
 
     global MAX_PLAYER,MIN_PLAYER
-    with open(sys.argv[1]) as inputFile:
+    with open(sys.argv[2]) as inputFile:
 
         task = inputFile.readline().strip()
-        print "task:",task
 
         line = inputFile.next()
         player_id = line.strip()
-        print "player_id:",player_id
 
         if player_id == '1':
             MAX_PLAYER = 1
@@ -750,26 +570,21 @@ def ParseInputFile():
 
         line = inputFile.next()
         cutoff = line.strip()
-        print "cutoff:",cutoff
 
         line = inputFile.next()
         p2_states = line.strip().split()
-        #print p2_states
         A = p2_states
 
         line = inputFile.next()
         p1_states = line.strip().split()
-        #print p1_states
         B = p1_states
 
         line = inputFile.next()
         p2_mancala = line.strip()
-        #print p2_mancala
         A.insert(0,p2_mancala)
 
         line = inputFile.next()
         p1_mancala = line.strip()
-        #print p1_mancala
         B.append(p1_mancala)
 
         A = map(int,A)
@@ -777,24 +592,22 @@ def ParseInputFile():
 
         return A,B,int(cutoff),int(task)
 
+
 def main():
     A,B,cutoffDepth,task = ParseInputFile()
 
-    global MAX_PLAYER,MIN_PLAYER
+    global MAX_PLAYER,MIN_PLAYER,isGreedy
 
     Start = GameState(A,B)
-    #printTraverseLog_alpha_beta('Node','Depth','Value','','Alpha','Beta')
-    #Minimax(Start,2,3)    ####problem with >3
 
     if task == 1:
+        isGreedy = True
         Minimax(Start,MAX_PLAYER,1)
     elif task == 2:
         Minimax(Start,MAX_PLAYER,cutoffDepth)
     else:
         AlphaBeta(Start,MAX_PLAYER,cutoffDepth)
 
-    #Minimax(Start,MAX_PLAYER,3)
-    #AlphaBeta(Start,MAX_PLAYER,2)
 
 if __name__ == '__main__':
     main()
